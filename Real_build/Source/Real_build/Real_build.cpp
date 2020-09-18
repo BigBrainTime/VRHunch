ef471b66d75956f52fc44d1421f0061047d64a4e
bandinnite <57802021+bandinnite@users.noreply.github.com>
2020-09-18 09:00:45 -0500

    slow add 1

---
+++ Real_build/Source/Real_build/Real_build.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Real_build.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Real_build, "Real_build" );
---
+++ Real_build/Source/Real_build/Real_buildGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Real_buildGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REAL_BUILD_API AReal_buildGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
